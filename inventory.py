99c7c9c36bb2d799004b256633a4fb0f9dbb9af8
amol27695 <amol27695@gmail.com>
2025-06-30 09:16:52 +0530

    Create inventory.py

---
+++ inventory.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import json
+
+def generate_inventory():
+    inventory = {
+        "web": {
+            "hosts": [
+                "serverb.lab.example.com"
+            ]
+    },
+        "_meta": {
+            "hostvars": {
+                "serverb.lab.example.com": {
+                    "ansible_host": "serverb.lab.example.com",
+                    "ansible_user": "root"
+         }
+     }
+  }
+}
+    return inventory
+
+if__name__ == "__main__":
+    print(json.dumps(generate_inventory(), indent=))
